b620de3c8925514afc08f28737c4aab460ac2427
Vinicius <vinibol12@yahoo.com.br>
2015-07-02 19:32:28 +1200

    asset compilation in production disabled.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,10 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.compress = true
+  config.assets.js_compressor = NoCompression.new
+  config.assets.css_compressor = NoCompression.new
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
